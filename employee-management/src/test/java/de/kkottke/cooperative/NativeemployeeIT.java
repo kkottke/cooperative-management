6eb27a71be7864bd1aa6927024daddca42d9d4bc
Kristian Kottke <Kristian.Kottke@iteratec.de>
2020-03-06 15:37:10 +0100

    Initial commit.

---
+++ employee-management/src/test/java/de/kkottke/cooperative/NativeemployeeIT.java
@@ -0,0 +1,9 @@
+package de.kkottke.cooperative;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeemployeeIT extends employeeTest {
+
+    // Execute the same tests but in native mode.
+}
